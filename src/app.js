aa2392d8bb46ccbc87684e8f59428daadd2e2459
Guillermo Sarmiento <niko_sarmiento95@hotmail.com>
2025-06-10 19:45:33 -0300

    Actualizacion de arhcivos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 
 import usersRouter from "./routes/users.router.js";
 import petsRouter from "./routes/pets.router.js";
@@ -12,12 +13,23 @@ import mockingRouter from "./routes/mocking.router.js";
 import logger from "./utils/logger.js";
 import loggerRouter from "./routes/logger.router.js";
 
+const envFile =
+  process.env.NODE_ENV === "production" ? ".env.prod" : ".env.dev";
+dotenv.config({ path: envFile });
+
 const app = express();
 const PORT = process.env.PORT || 8080;
-const connection = mongoose.connect(`URL DE MONGO`);
+const connection = mongoose.connect(process.env.MONGODB_URI);
 
 app.use(express.json());
 app.use(cookieParser());
+
+// middleware para logger de peticiones
+app.use((req, res, next) => {
+  req.logger = logger;
+  logger.http(`${req.method} ${req.url}`);
+  next();
+});
 
 app.use("/api/users", usersRouter);
 app.use("/api/pets", petsRouter);
@@ -27,19 +39,13 @@ app.use("/api/sessions", sessionsRouter);
 app.use("/api", mockingRouter);
 app.use((err, req, res, next) => {
   // middleware para manejo de errores
-  req.logger.error(error.message);
+  req.logger.error(err.message);
   res.status(500).json({
     status: "error",
     message: "Algo salió mal en el servidor",
     code: err.code || 500,
     cause: err.cause || "Error no manejado",
   });
-});
-// middleware para logger de peticiones
-app.use((req, res, next) => {
-  req.logger = logger;
-  logger.http(`${req.method} ${req.url}`);
-  next();
 });
 
 // ruta de prueba para logger
